18020c07f8ba708112caf965ecd6381a05d2cb4c
tortrakul <Tortrakul_C@cmu.ac.th>
2015-05-17 13:49:04 +0700

    UI

---
+++ WatDuangDee/WatDuangDee/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WatDuangDee.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult History()
+        {
+            return View();
+        }
+        public ViewResult News()
+        {
+            return View();
+        }
+        public ViewResult Activity()
+        {
+            return View();
+        }
+        public ViewResult QA()
+        {
+            return View();
+        }
+        public ViewResult Dhamma()
+        {
+            return View();
+        }
+        public ViewResult Photo()
+        {
+            return View();
+        }
+        public ViewResult Video()
+        {
+            return View();
+        }
+        public ViewResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
